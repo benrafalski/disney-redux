3eb565c0a5a0c6e81d016b46407e09147d89d2c6
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-08-10 17:47:43 -0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Login from './components/Login';
-
+import Header from './components/Header';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+ 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path='/'>
+            <Login/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,161 @@
+import styled from "styled-components"
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/Add';
+import StarIcon from '@material-ui/icons/Star';
+import TheatersIcon from '@material-ui/icons/Theaters';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+
+const Header = () => {
+    return (
+        <Nav>
+            <Logo>
+                <img src='https://cannonball-cdn.bamgrid.com/assets/originals/logo-nopad.svg' alt='Disney+'/>
+            </Logo>
+            <NavMenu>
+                <a href='/home'>
+                    <HomeIcon/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <SearchIcon/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <AddIcon/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <StarIcon/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <TheatersIcon/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <LiveTvIcon/>
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <Login>LOGIN</Login>
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    transition: opacity .5s ease-out;
+    letter-spacing: 16px;
+    z-index: 3;
+`;
+
+const Logo = styled.a`
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    transition: all .2s ease 0s;
+    font-size: 0;
+    display: inline-block;
+
+    img {
+        display: block;
+        width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            margin-left: 8px;
+            padding: 2px 0px;
+            white-space: nowrap;
+            position: relative;
+
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -6px;
+                content: '';
+                height: 2px;
+                left: 0px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+        &:hover {
+            span:before {
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1 !important;
+            }
+        }
+    }
+
+    @media (max-width: 768px){
+        display: none;
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease 0s;
+
+    &:hover{
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
+`;
+
+
+
+export default Header
